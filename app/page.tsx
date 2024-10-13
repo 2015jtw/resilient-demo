a8a7c41e9ec4f3ef78c1b0171c088229254abbfb
Josh Worden <2015JTW@gmail.com>
2024-10-13 20:19:42 +0200

    trying to configure resilientURL wp data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
 export default async function Home() {
-  const res = await fetch(
-    "https://public-api.wordpress.com/wp/v2/sites/brisklyperfect673e684357.wordpress.com/posts/10"
-  );
-  const post = await res.json();
-  console.log("post: ", post);
+  // testURL = https://wordpress.com/posts/brisklyperfect673e684357.wordpress.com
+  // resilientURL = https://wordpress.com/posts/resilient.llc
+
+  // const res = await fetch(
+  //   "https://public-api.wordpress.com/wp/v2/sites/brisklyperfect673e684357.wordpress.com/posts/10"
+  // );
+
+  try {
+    const res = await fetch(
+      "https://public-api.wordpress.com/wp/v2/sites/resilient.llc/posts/33"
+    );
+
+    const post = await res.json();
+    console.log("post: ", post);
+  } catch (err) {
+    console.log("error: ", err);
+  }
+
   return (
     <div className="">
       <h1>Resilient Demo</h1>
-      <p>Post ID: {post.id}</p>
+      {/* <p>Post ID: {post.id}</p>
       <h2>{post.title.rendered}</h2>
-      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} /> */}
     </div>
   );
 }
