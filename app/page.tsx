ca0597a170826df120c0162931447e9c5a131357
Josh Worden <2015JTW@gmail.com>
2024-10-17 10:36:46 +0200

    created hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,20 @@
-"use client";
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import HeroSection from "@/components/Hero-Section";
+
 export default function Home() {
-  const [page, setPage] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [imageUrl, setImageUrl] = useState(""); // State for image URL
-  const [title, setTitle] = useState(""); // State for title
-  const [description, setDescription] = useState(""); // State for description
-  const [desc2, setDesc2] = useState("");
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const res = await fetch(
-          "https://public-api.wordpress.com/wp/v2/sites/resilient.llc/pages/53"
-        );
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch the post data");
-        }
-
-        const data = await res.json();
-        console.log("data: ", data.content.rendered);
-        // console.log("home page data: ", data.content.rendered);
-        setPage(data); // Set the fetched data
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data.content.rendered, "text/html");
-
-        const imgElement = doc.querySelector(
-          ".wp-block-cover__image-background"
-        );
-        const titleElement = doc.querySelector(".wp-block-heading");
-        const descriptionElement = doc.querySelector("p.has-text-color");
-        const descContent = doc.querySelector(".wp-block-quote p");
-
-        if (imgElement) {
-          setImageUrl(imgElement.src); // Extract the image URL
-        }
-        if (titleElement) {
-          setTitle(titleElement.textContent); // Extract the title text
-        }
-
-        if (descriptionElement) {
-          setDescription(descriptionElement.textContent); // Extract the description text
-        }
-        if (desc2) {
-          setDesc2(descContent?.textContent);
-          console.log("show me: ", descContent);
-        }
-      } catch (err) {
-        setError(err.message); // Catch any errors and set the error state
-      } finally {
-        setLoading(false); // Set loading to false after the fetch is complete
-      }
-    };
-
-    fetchPost(); // Call the fetch function when the component mounts
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>; // Display a loading message while fetching data
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>; // Display an error message if an error occurs
-  }
-
-  if (!page) {
-    return <div>No post data available</div>; // Display a message if no post data is available
-  }
-
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto">
       <div className="my-5">
-        <h1>Routing for Navigation Menu</h1>
-        <p id="home">Home</p>
-        <p id="about-us">About Us</p>
-        <p id="blog">Blog</p>
-        <p id="contact-us">Contact</p>
+        <h1 className="text-center pb-4 font-semibold text-2xl">
+          Routing for Navigation Menu
+        </h1>
+        <div className="flex justify-center gap-4">
+          <p id="home">Home</p>
+          <p id="about-us">About Us</p>
+          <p id="blog">Blog</p>
+          <p id="contact-us">Contact</p>
+        </div>
       </div>
-
-      <div className="my-5">
-        <h1 className="text-xl">Grab Post Wordpress Test</h1>
-        <h1 className="text-xl">{title}</h1> {/* Display the extracted title */}
-        {imageUrl && (
-          <div className="h-10">
-            <Image
-              src={imageUrl}
-              alt="homepage image"
-              width={800}
-              height={600}
-            />
-          </div>
-        )}
-        <p>{description}</p> {/* Display the extracted description */}
-        <p>{desc2}</p>
-      </div>
+      <HeroSection />
     </div>
   );
 }
---
+++ components/Hero-Section.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function HeroSection() {
+  return (
+    <div className="relative isolate flex items-center justify-center h-[600px]">
+      <Image
+        src="/images/hero-section.jpg"
+        alt="forest"
+        fill
+        objectFit="cover"
+      />
+      <div className="mx-auto max-w-4xl w-full relative z-10">
+        <div className="relative mx-auto px-10 md:px-0 max-w-2xl">
+          <div className="text-center">
+            <p className="text-sm text-white">
+              Our overarching mission is to help strengthen your capabilities.
+            </p>
+            <h1 className="text-4xl font-bold tracking-tight text-yellow sm:text-6xl">
+              Resilient LLC
+            </h1>
+            <p className="my-6 text-lg font-medium leading-8 text-white">
+              The COVID-19 pandemic was a wake-up call to leadership that
+              preparedness matters.
+            </p>
+            <Button>
+              <Link href="#" className="underline-offset-4 hover:underline">
+                Get in Touch
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
